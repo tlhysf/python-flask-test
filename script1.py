37a15596bbdf2c679aa449dfe7e7cb92cc847338
tlhysf <47601633+tlhysf@users.noreply.github.com>
2020-04-06 09:21:56 +0500

    first commit

---
+++ script1.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/1')
+def page1():
+    return render_template("page1.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
